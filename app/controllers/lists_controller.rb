7a6bd9f091a7a0877a4a073a7bb0354e92ed9ceb
Shanti Ravdjee <shanti.ravdjee@gmail.com>
2023-02-23 15:22:00 +0100

    Features

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+
+end
